19eabe91a39d96d5659cfb9a87e9189c2b28c6e8
ghayth31 <gharsallighaith31@gmail.com>
2023-12-12 11:34:34 +0100

    hey html in flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+
+@app.route('/')
 def hello():
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
-@app.route("/hbnb", strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_c(text):
-    return f"C {text.replace('_', ' ')}"
 
-@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def display_python(text):
-    return f"Python {text.replace('_', ' ')}"
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def display_number(n):
-    return f"{n} is a number"
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def display_number_template(n):
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
     return render_template('5-number.html', n=n)
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
